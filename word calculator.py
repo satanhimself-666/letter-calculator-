3640d11f3f68d80f56671c3a895a18fdaee6b713
satanhimself-666 <84249581+satanhimself-666@users.noreply.github.com>
2021-06-02 13:23:02 +0530

    Add files via upload

---
+++ word calculator.py
@@ -0,0 +1,76 @@
+
+
+print("°•°•°•°•°•°•°•°•°•°•°•°•°°°•°•°•°•°•°•°•°•°•°•°•°°•°•°°°")
+
+print ("HI FRIEND.here you can find; \n\nhow much specific letter \
+is in your given text and \nthe total percentage of the letter")
+print ("\n");print("***********************************************************")
+
+#welcome message is completed 
+#next step is talking input from user 
+
+print ("\n");print ("\n")
+text=input("◇◇◇◇◇◇enter your text message◇◇◇◇◇◇\n\n")
+print ("\n");print("____________________________________");print ("\n")
+
+a=1
+
+while a==1:
+
+     letter=input ("◇◇◇◇enter the letter for counting◇◇◇◇")
+
+
+
+     print ("\n");print("____________________________________");print("\n")
+
+     count=text.count(letter)
+
+     textlength=len(text)
+
+     percentage =count/textlength*100
+
+#calculation is done..now we have to print the results ..
+
+     print ("the letter",letter,"is contains",count,"times in the text " )
+
+     print ("\n")
+
+
+     print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+     print ("\n")
+
+     print ("the percentage is ",percentage )
+
+     print(":::::::::::::::::::::::::::::::::::::")
+
+     print("thank you for using this program \n have a nice day \
+happy coding guys")
+
+     print ("\n")
+
+     print ("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
